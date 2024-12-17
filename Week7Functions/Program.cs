532f494e9ad274ffefe368519698a2a3506ec7fb
Veniamin <benjaminshapkin@gmail.com>
2024-12-17 18:49:44 +0200

    week7

---
+++ Week7Functions/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Are you coming or leaving? (in/out):");
+string userChoise = Console.ReadLine();
+
+if(userChoise == "in")
+{
+    PrintHello();
+}
+else
+{
+    PrintGoodBye();
+}
+
+
+static void PrintHello() //function / method
+{
+    Console.WriteLine("Hello, world!");
+}
+
+static void PrintGoodBye()
+{
+    Console.WriteLine("See you later, aligator.");
+}
